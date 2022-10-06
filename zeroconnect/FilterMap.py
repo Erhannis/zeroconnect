7bd327cb31702f11d237de7f436d6c2e967cbec9
Erhannis Kirran <eyeillus@gmail.com>
2022-10-06 14:53:29 -0400

    Went the slow but simple route.  Sigh.  I probably should have done that to begin with.

---
+++ zeroconnect/FilterMap.py
@@ -0,0 +1,89 @@
+#TODO Export
+class FilterMap:
+    def __init__(self, size):
+        """
+        Addressed by tuples.  `size` is size of tuple. 
+        Get can contain Nones.
+        Set *can* contain Nones, but it's not recommended - I suspect it'll be a little messy.
+        getExact returns a single entry, or throws.
+        getFilter returns a list of matching entries.
+        Get performs getExact or getFilter, depending on whether the key contains Nones.
+
+        fm = FilterMap(3)
+        fm[(0,0,0)] = 0
+        fm[(0,0,1)] = 1
+        fm[(0,1,0)] = 2
+        fm[(0,1,1)] = 3
+        fm[(1,0,0)] = 4
+        fm[(1,0,1)] = 5
+        fm[(1,1,0)] = 6
+        fm[(1,1,1)] = 7
+
+        fm[(1,1,1)]    # = 7
+        fm[(1,1,None)] # = [6, 7]
+
+        I abandoned fast-lookup; now if the key contains Nones it iterates the map.  Sigh.
+        ...I probably shouldn't put commentary in my documentation.
+        """
+        self.map = {}
+        self.size = size
+    
+    def __len__(self):
+        return len(self.map)
+    
+    def getExact(self, key):
+        """
+        Can be used to retrieve entries that have None in them.
+        """
+        return self.map[key]
+
+    def getFilter(self, key):
+        results = []
+        for k in self.map:
+            bad = False
+            for i in range(self.size):
+                if key[i] != None and k[i] != key[i]:
+                    bad = True
+                    break
+            if bad:
+                continue
+            results.append(self.map[k])
+        return results
+
+    def __getitem__(self, key):
+        hasNone = False
+        for i in range(self.size):
+            if key[i] == None:
+                hasNone = True
+                break
+        if hasNone:
+            return self.getFilter(key)
+        else:
+            return self.getExact(key)
+
+    def __setitem__(self, key, value):
+        self.map[key] = value
+
+    def __delitem__(self, key):
+        """
+        Deletes key.  Exact match.
+        #TODO Permit filter delete?
+        """
+        del self.map[key]
+
+    def delFilter(self, key):
+        keys = []
+        for k in self.map:
+            bad = False
+            for i in range(self.size):
+                if key[i] != None and k[i] != key[i]:
+                    bad = True
+                    break
+            if bad:
+                continue
+            keys.append(k)
+        for k in keys:
+            del self.map[k]
+
+    def __iter__(self):
+        return self.map.__iter__()
